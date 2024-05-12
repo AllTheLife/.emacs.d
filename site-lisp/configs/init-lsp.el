927f71647b88b7cf10faa8e41a5cb8b18bf8d431
was <xjn208930@gmail.com>
2024-05-12 19:12:57 +0800

    Ready or not?

--- site-lisp/configs/init-lsp.el
+++ site-lisp/configs/init-lsp.el
@@ -12,6 +12,7 @@
 (setq acm-backend-yas-candidate-min-length 2)
 (setq acm-backend-lsp-candidate-min-length 2)
 (setq acm-backend-lsp-enable-auto-import nil)
+(setq acm-backend-lsp-match-mode 'regexp-quote)
 (setq lsp-bridge-enable-org-babel nil)
 (setq lsp-bridge-c-lsp-server "ccls")
 (setq lsp-bridge-enable-hover-diagnostic t)
@@ -41,6 +42,50 @@
     (lsp-bridge-peek)
     (setf (nth 3 lsp-bridge-peek-ace-list) (buffer-name))
     (setf (nth 4 lsp-bridge-peek-ace-list) (point))))
+
+(progn
+  (defun exec/lsp-which-function(file line column)
+	(with-current-buffer
+		(find-file-noselect file)
+	  (goto-line line)
+	  (move-to-column column)
+      (which-function)))
+
+  (defun get-function-name-and-overlay (file line column)
+	"Find the function name at a specific line and column in a file and put an overlay."
+	(let* ((function-name (exec/lsp-which-function file line column))
+		   (ov (make-overlay (line-beginning-position) (1+ (line-beginning-position)) nil t))
+		   (text (format "%30s │" (if function-name function-name "")))
+		   )
+	  ;; (delete-all-overlays (current-buffer))
+      (overlay-put ov 'before-string
+				   (propertize text 'face 'font-lock-string-face)
+				   )
+	  (overlay-put ov 'evaporate t)))
+
+  (defun parse-buffer-and-overlay-function-name (&optional a b c)
+	"Parse the buffer content to get file, line number, column and make an overlay of function name."
+	(interactive)
+	(save-excursion
+      (goto-char (point-min))
+      (let ((current-file nil))
+		(while (not (eobp)) ; while not end of buffer
+          ;; check if current line is a file path
+          (if (looking-at "^/.+?$")
+              ;; update the current file
+			  (setq current-file (buffer-substring-no-properties
+								  (line-beginning-position) (line-end-position)))
+			(progn
+			  ;; else check if it's a line:col
+			  (when (and current-file (looking-at "^\\([0-9]+\\):\\([0-9]+\\):"))
+				;; call your function with the captured groups as arguments
+				(get-function-name-and-overlay
+				 current-file
+				 (string-to-number (match-string 1))
+				 (string-to-number (match-string 2))))))
+          (forward-line 1)))))
+
+  (advice-add 'lsp-bridge-references--popup :after 'parse-buffer-and-overlay-function-name))
 
 
 (provide 'init-lsp)
